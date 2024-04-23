5e0ced1b5e8aa912b77218a6a7a195e0a4148f15
elonathaqi <eelonathaqi@gmail.com>
2024-04-23 11:26:41 +0200

    added login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { About } from './components/About'
+import LoginForm from './components/Login'
 import { Product } from './components/Product'
 import { Products } from './components/Products'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<Products />}/>
           <Route path='/product/:id' element={<Product />}/>
           <Route path='/about' element={<About />}/>
+          <Route path='/login' element={<LoginForm />}/>
         </Routes>
       </BrowserRouter>
       
